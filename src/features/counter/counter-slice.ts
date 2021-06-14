3fa43473d90ea19ef8e9f32c73c1d0609b89f6ab
garyfoo88 <reid9000200020002000@gmail.com>
2021-06-14 17:22:34 +0800

    first commit

---
+++ src/features/counter/counter-slice.ts
@@ -0,0 +1,33 @@
+// DUCKS pattern
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    incremented(state) {
+      // it's okay to do this because immer makes it immutable
+      // under the hood
+      state.value++;
+    },
+    amountAdded(state, action: PayloadAction<number>) {
+      state.value += action.payload;
+    },
+    decremented(state) {
+      state.value--;
+    }
+    // decrement
+    // reset
+  },
+});
+
+export const { incremented, amountAdded, decremented } = counterSlice.actions;
+export default counterSlice.reducer;
